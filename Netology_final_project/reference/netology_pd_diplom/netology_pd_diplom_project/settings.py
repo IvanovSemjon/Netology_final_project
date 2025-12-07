10bfdfa2451c5c72aeba1fb57f1c989b33af5d33
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-12-07 16:13:00 +0300

    Поменял структуру, вынес информацию для преподователя в корневую папку + добавил докер/докеркомпос

--- Netology_final_project/reference/netology_pd_diplom/netology_pd_diplom_project/settings.py
+++ Netology_final_project/reference/netology_pd_diplom/netology_pd_diplom_project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+DEBUG = os.environ.get("DJANGO_ENV", "dev") == "dev"
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
