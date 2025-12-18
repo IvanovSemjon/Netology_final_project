62211fca6089972748ec0ab3ce36ad682eb1eba8
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-12-18 18:15:02 +0300

    Удалил лишний импорт синхронности

--- Netology_final_project/reference/netology_pd_diplom/backend/api/views/auth.py
+++ Netology_final_project/reference/netology_pd_diplom/backend/api/views/auth.py
@@ -7,7 +7,6 @@ from rest_framework.views import APIView
 
 from backend.models import User, ConfirmEmailToken
 from backend.api.serializers.user import UserSerializer
-from backend.services.emails import send_confirmation_email
 
 
 class RegisterAccount(APIView):
