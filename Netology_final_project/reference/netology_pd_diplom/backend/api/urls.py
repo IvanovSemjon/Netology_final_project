f6149ed0e79d18a405e06a103746a2e39a19639e
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-12-23 09:41:42 +0300

    Корректировки urls + readme

--- Netology_final_project/reference/netology_pd_diplom/backend/api/urls.py
+++ Netology_final_project/reference/netology_pd_diplom/backend/api/urls.py
@@ -32,15 +32,15 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    # API Root
+    # =======  API Root =====================
     path('', api_root, name='api-root'),
-    # Partner endpoints
+    # =======  Partner endpoints ============
     path('partner/update/', PartnerUpdate.as_view(), name='partner-update'),
     path('partner/state/', PartnerState.as_view(), name='partner-state'),
     path('partner/orders/', PartnerOrders.as_view(), name='partner-orders'),
     path('partner/orders/status/', PartnerOrders.as_view(), name='partner-orders-status'),
     
-    # User endpoints
+    # =======  User endpoints ===============
     path('user/register/', RegisterAccount.as_view(), name='user-register'),
     path('user/register/confirm/', ConfirmAccount.as_view(), name='user-register-confirm'),
     path('user/details/', AccountDetails.as_view(), name='user-details'),
@@ -49,16 +49,16 @@ urlpatterns = [
     path('user/password_reset/', reset_password_request_token, name='password-reset'),
     path('user/password_reset/confirm/', reset_password_confirm, name='password-reset-confirm'),
     
-    # Catalog endpoints
+    # =======  Catalog endpoints ============
     path('categories/', CategoryView.as_view(), name='categories'),
     path('shops/', ShopView.as_view(), name='shops'),
     path('products/', ProductInfoView.as_view(), name='products'),
     
-    # Order endpoints
+    # =======  Order endpoints ==============
     path('basket/', BasketView.as_view(), name='basket'),
     path('order/', OrderView.as_view(), name='order'),
     
-    # Admin endpoints
+    # =======  Admin endpoints ==============
     path('admin/import/', AdminImportView.as_view(), name='admin-import'),
     # path('admin/start-import/', start_import, name='admin-start-import'),
 ]
